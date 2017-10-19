14b3c8f4f938700e6dbbbc988ce548b73bd1eb21
Xiaoming op de Hoek <xpopdehoek@gmail.com>
2017-10-19 18:26:27 +0200

    Minor tweaks and thoroughly checked nm-folder Minor tweaks have to do with variable order, which happened to be important. Thoroughly checke nm-folder.js functions and next work is on nm-file.js.
    Think of a solution to get the variables of path just right, since these
    have to be re-initialized whenever dreamweaver insert an object. This is
    due to all dreamweaver tabs sharing this script file.
    
    Solutions to this problem could be immediate invocation/closure or just
    an var_init function where the values will be initiated globally, this
    could also prove to be a problem when we take advantage of the
    persistend of objects and variables...

--- dev/nm-file.js
+++ dev/nm-file.js
@@ -7,16 +7,16 @@ var REGEX_EDGEFILE = /\_edge\.js/;
  * @returns {(string | boolean)} If file exists returns string, else return false.
  */
 function nemo_getAnimeName(folderpath) {
-    var file_list = DWfile.listFolder(folderPath, "files");
+    var file_list = DWfile.listFolder(folderPath, "files"); //check the typeof the output...
     var error = false;
 
-    if (list) {
+    if (file_list.length) {
         
         var numValidEdgeAnime = 0;
 
-        for (var i = 0; i < list.length; i++) {
+        for (var i = 0; i < file_list.length; i++) {
             
-            var element = list[i];
+            var element = file_list[i];
             
             if (element.test(REGEX_EDGEFILE)) {numValidEdgeAnime++}
         }
@@ -31,6 +31,8 @@ function nemo_getAnimeName(folderpath) {
 
             error = "There does not exist an unique Edge Animation file.\nMake sure to save an Edge Animation in it's own folder.";
         }
+    } else {
+        error = "There are not even files present in this folder...,\nMake sure to select an Edge Animation Folder,\nor publish (Ctrl+Alt+S) your edge animation first.";
     }
 
     if (error) {
--- dev/nm-folder.js
+++ dev/nm-folder.js
@@ -4,32 +4,73 @@
  * NOTE: All the functions and idea assume the standard nemo-folder structure.
  */
 
-// local variables for all functions
-var REGEX_PATH = /^(.*[\\\/])/;                                                 // everything from begin up to last '/'.
-var REGEX_PUBLISHWEB = /publish\/web/;
-var REGEX_PUBLISH = /publish/;                                               
-var REGEX_NEMOWEBFOLDER = /(_web)[\s\S]*/;                                      // everything from '_web' to end.
-
-// Due to size do not use dw.getDocumentDOM().URL
-var abs_doc_path = dw.getDocumentPath('document');                              // current document.  URL_path ends with 'file.html', dw API: docPathURL
-var abs_folder_path = nm.getFolderPath(abs_doc_path);                           // current folder.    URL_path ends with '/'
-var abs_root_path = abs_folder_path.replace(REGEX_NEMOWEBFOLDER, '');           // topic (NL: module) folder. URL_path ends with '/', dw API: siteRootURL
-
+// local variables for all functions (ORDER MATTERS)
 var file_edgejs = '_edge.js';                                                   // typical part of filename of animation.
 var folder_publishweb = '/publish/web';                                        // First character is '/' due to dw.browseForFolderURL();
 var folder_web = '/web';                                                       // First character is '/' due to dw.browseForFolderURL();
-var folder_anime_src = 'animations_src'; 
+var folder_anime_src = '/animations_src';
+var path_delimiter = '/';
+var nemoweb_delimiter = '\/_web'; 
+
+//var REGEX_PATH = /^(.*[\\\/])/;                                                 // everything from begin up to last '/'.
+var REGEX_TRAILING_PUBLISHWEB = /publish\/web$/;
+var REGEX_TRAILING_PUBLISH = /publish$/;                                               
+//var REGEX_NEMOWEBFOLDER = /(\/_web)[\s\S]*/;                                      // everything from '_web' to end.
+
+// local variables initialized by functions,
+// Due to size do not use dw.getDocumentDOM().URL
+
+// !WARNING! THESE PATH VALUES HAVE TO BE INITIATED FOR EVERY insertObject,
+// THIS CODE IS SHARED WITH ALL TABS IN DREAMWEAVER.
+var abs_doc_path = dw.getDocumentPath('document');                              // current document.  URI_path ends with 'file.html', dw API: docPathURL
+var abs_folder_path = nemo_getParentFolderPath(abs_doc_path);                   // current folder.    URI_path ends with '/'
+var abs_root_path = nemo_getStringSliceUpTo(abs_folder_path, nemoweb_delimiter);           // topic (NL: module) folder. URI_path ends with folder name, dw API: siteRootURL
 
 /**
  * Checks if input folder is an eligible animation folder, i.e. is published
+ * If it is eligible it will return the published folder.
  * 
- * @param {string} folderPath 
- * @returns {boolean} If eligible returns true, else false.
+ * @param {string} folderPath - absolute folder URI
+ * @example
+ * // returns (if valid): "file:///C:/.../vermogen_10/publish/web"
+ * nemo_isValidAnimeFolder("file:///C:/.../vermogen_10");
+ * @example
+ * // returns (if valid): "file:///C:/.../vermogen_10/publish/web"
+ * nemo_isValidAnimeFolder("file:///C:/.../vermogen_10/publish");
+ * @example
+ * // returns (if valid): "file:///C:/.../vermogen_10/publish/web"
+ * nemo_isValidAnimeFolder("file:///C:/.../vermogen_10/publish/web");
+ * @example
+ * // returns (if unvalid): false
+ * nemo_isValidAnimeFolder(true);
+ * @returns {string | boolean} If eligible returns path, else false.
  */
 function nemo_isValidAnimeFolder(folderPath) {
+    try {
+        if (typeof folderPath !== 'string' && typeof folderPath != 'boolean') {
+            throw "ERROR: Wrong input; nemo_isValidAnimeFolder's argument is of type 'string' or 'boolean'.";
+        }
+    } catch(err) {
+        alert(err);
+    }
 
-    if (DWfile.exists(folderPath)) {
-        return true;
+    if (folderPath === "" || typeof folderPath === 'boolean') {
+        publish_path = false;
+    } else if (REGEX_TRAILING_PUBLISHWEB.test(folderPath)) {
+        // it is a ".../publish/web/ path
+        publish_path = folderPath;
+    } else if (REGEX_TRAILING_PUBLISH.test(folderPath)) {
+        // it is a ".../publish/" path
+        publish_path = folderPath + folder_web;
+    } else {
+        publish_path = folderPath + folder_publishweb;
+    }
+
+    // Condition makes it possible that publish_path can be false,
+    // alert message can be defined at one location, instead of multiple.
+    if (publish_path && dw.exists(publish_path)) {
+    //if (publish_path && DWfile.exists(publish_path)) {
+        return publish_path;
     } else {
         alert("Could not find published folder.\nPlease publish (Ctrl+Alt+S) your animation first.");
         return false;
@@ -37,78 +78,116 @@ function nemo_isValidAnimeFolder(folderPath) {
 }
 
 /**
- * Extracts the absolute path from the input (<tt>path</tt>), from beginning up to the last '/'.
+ * Takes everything up to (not including) the last delimiter as substring. 
+ * If any input is of the wrong type, it outputs the original <tt>in_string</tt>.
+ * 
+ * @param {string} in_string - a string
+ * @param {string} delimiter - a delimiter
+ * @example
+ * // returns "file:///C:/name/_web/name"
+ * nemo_getStringSliceUpTo("file:///C:/name/_web/name/_web/anything". "\/_web");
+ * @example
+ * // returns 345, which is a number!
+ * nemo_getStringSliceUpTo(345, "3");
+ * @example
+ * // returns "file:///C:/name/_web/name"
+ * nemo_getStringSliceUpTo("file:///C:/name/_web/name", "xp");
+ * @return {string} A substring/slice of the original string. 
+ */
+function nemo_getStringSliceUpTo(in_string, delimiter) {
+    if (typeof in_string === 'string' && typeof delimiter === 'string') {
+        out_string = in_string.slice(0, in_string.lastIndexOf(delimiter));
+    } else {
+        alert("ERROR: Wrong input type, make sure the input type is 'string'.");
+        out_string = in_string;
+    }
+
+    return out_string;
+}
+
+/**
+ * Extracts the absolute path from the input (<tt>path</tt>), from beginning up to (not including) the last '/'.
+ * If given a URI it thus returns the URI of the parent folder.
  *
  * @param {string} [path = dw.getDocumentPath('document')] - input folder or file path.
  * @example
- * // returns "file://C:/Name/folder/"
- * nemo_getFolderPath("file://C:/Name/folder/name.txt");
+ * // returns "file:///C:/Name/folder"
+ * nemo_getParentFolderPath("file:///C:/Name/folder/name.txt");
  * @example
- * // returns "file://C:/Name/folder/"
- * nemo_getFolderPath("file://C:/Name/folder/")
- * @returns {string} A folder path ("file://.../").
+ * // returns "file:///C:/Name"
+ * nemo_getParentFolderPath("file:///C:/Name/folder");
+ * @example
+ * // returns "file:///C:/Name/folder"
+ * nemo_getParentFolderPath("file:///C:/Name/folder/")
+ * @returns {string} A folder path ("file:///...").
  */
-function nemo_getFolderPath(path) {
+function nemo_getParentFolderPath(path) {
     if (path == null) {
         folderPath = abs_doc_path;
     } else {
+
+        try {
+            if (typeof path !== 'string') throw "is not of type string.";
+            //if (!DWfile.exists(path)) throw "is not an existing URIs."; // Should be active in the end product...
+        } catch(err) {
+            alert("ERROR: nm_getFolderPath's argument " + err);
+        }
+
         folderPath = path;
     } // can also use value = thing1 || default (if thing1, else default);
 
-    // makes sure to return only one value.
-    folderPath = folderPath && REGEX_PATH.exec(folderPath);
-    folderPath = folderPath.length > 0 && folderPath[0];
-    return folderPath;
+    return nemo_getStringSliceUpTo(folderPath, path_delimiter);
 }
 
 /**
  * Asks the user to select a folder containing Edge Animate animations.
  * 
- * @param {string} startFolder
+ * @param {string} [startFolder = "/animations_src"] - starting folder, relative to abs_root_path
  * @example
- * // if no user input, then returns ""; else returns "file://.../foldername";
+ * // if no user input, then returns false; else returns "file:///.../foldername";
  * nemo_getFolderPathBrowse("animations_src")
- * @returns {string} An absolute folder path which can be empyt ("") or full ("file://.../foldername").
+ * @returns {string | boolean} a path ("file:///.../foldername") or false when nothing is selected.
  */
 function nemo_getFolderPathBrowse(startFolder) {
-    var startURL = abs_root_path + startFolder + '/';
-    
-    var absFolderPath = dw.doURLEncoding(dw.browseForFolderURL("Please select an animation folder.", startURL));
+    if (startFolder == null) {
+        startFolder = folder_anime_src;
+    }
+    var start_path = abs_root_path + startFolder;
+    var absFolderPath = "";
 
+    if (dw.exists(start_path)) {
+    // if (DWfile.exists(start_path)) {
+        absFolderPath = dw.browseForFolder();//dw.doURLEncoding(dw.browseForFolderURL("Please select an animation folder.", start_path));
+    } else {
+        alert("ERROR: nm.getFolderPathBrowse's given startFolder does not exist or is invalid input.");
+    }
+    
     // just to make sure it is absolute
     if (absFolderPath === "") {
-        return absFolderPath;
+        return false;
     } else {
-        return dw.relativeToAbsoluteURL(abs_doc_path, abs_folder_path, absFolderPath);
+        return absFolderPath;//dw.relativeToAbsoluteURL(abs_doc_path, abs_folder_path, absFolderPath);
     }
 }
 
 /**
- * Simple function that recursively asks the user to select a folder, 
+ * Simple function that recursively asks the user to do a task, 
  * until the user explicitly confirms it wants to quit.
  * It automatically adds the path to the published folder.
  * 
  * @param {string} message - confirmation message that is shown.
+ * @param {function} task - a function without arguments.
  * @example
- * nemo_loopFolderPathBrowse('Are you sure to quit?');
- * @returns {string} A string with the selected path, when not selected any it returns an empty string.
+ * // returns a nonempty URI or false.
+ * nemo_recurseTask('Are you sure to quit?', nemo_getFolderPathBrowse);
+ * @returns {string | boolean} A string with the selected path or false if the task is not completed.
  */
-function nemo_loopFolderPathBrowse(message) {
-    var path = nm.getFolderPathBrowse(folder_anime_src);
+function nemo_recurseTask(message, task) {
+    var output = task();
 
-    while (path === "" && !confirm(message)) {
-        path = nemo_getFolderPathBrowse(folder_anime_src);
+    while (!output && !confirm(message)) {
+        output = task();
     }
-    
-    if (path === "") {
-        return path;
-    } else if (REGEX_PUBLISH.test(path)) {
-        // it is a ".../publish/web/ path
-        return path;
-    } else if (REGEX_PUBLISH.test(path)) {
-        // it is a ".../publish/" path
-        publish_path = folderPath + folder_web;
-    } else {
-        publish_path = folderPath + folder_publishweb;
-    }
+
+    return output;
 }
