1c3c1015f2cc9cd45c243f3504fc1cda664946d6
Xiaoming op de Hoek <xpopdehoek@gmail.com>
2017-10-25 11:09:07 +0200

    Contains the actual changes from in the commit before this one

--- dev/Gruntfile.js
+++ dev/Gruntfile.js
@@ -1,21 +1,25 @@
-module.exports = function(grunt) {
-    
-   // This is the configuration.
-   grunt.initConfig({
-     concat: {
-       options: {
-         separator: '\n\n',
-       },
-       dist: {
-         src: ['src/json-part_nmscripts.js', 'src/cust-part_nmscripts.js'],
-         dest: 'dist/new_nmscripts.js',
-       },
-     },
-   });
-    
-     // Load the plugin that provides the "concat" task.
-     grunt.loadNpmTasks('grunt-contrib-concat');
-    
-     // Default task(s).
-     grunt.registerTask('default', ['concat']);
-   };
+module.exports = function (grunt) {
+
+  // This is the configuration.
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: '\n\n',
+      },
+      dist: {
+        src: ['src/json-part_nmscripts.js', 'src/cust-part_nmscripts.js'],
+        dest: 'dist/new_nmscripts.js',
+      },
+    },
+    copy: {
+
+    },
+  });
+
+  // Load the plugin that provides the "concat" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'copy']);
+};
