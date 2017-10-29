e4080aa397c44570d27925b4796ee3b76c7aba55
Xiaoming op de Hoek <xpopdehoek@gmail.com>
2017-10-29 14:10:49 +0100

    deletion

--- getpath.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-
-var dirpath = path.relative('C:/Users/Xiao Ming/olo/edgeanimationcontrol', 'C:/Users/Xiao Ming/AppData/Roaming/Adobe/Dreamweaver CC 2014.1/en_US/Configuration/Commands');
-
-console.log(dirpath);
